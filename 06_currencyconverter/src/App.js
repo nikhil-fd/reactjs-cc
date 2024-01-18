d021afd2cbf8fa02bb432fe4858ae780b9cb2c81
nikhil-fd <nikhildas.fd@gmail.com>
2024-01-18 12:26:31 +0530

    Made Currency Converter App convert from USD to INR

--- 06_currencyconverter/src/App.js
+++ 06_currencyconverter/src/App.js
@@ -30,7 +30,7 @@ function App() {
         className="w-full h-screen flex flex-wrap justify-center items-center
       bg-cover bg-no-repeat"
         style={{
-          backgroundImage: `url("https://images.pexels.com/photos/956981/milky-way-starry-sky-night-sky-star-956981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
+          backgroundImage: `url("https://images.pexels.com/photos/159888/pexels-photo-159888.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
         }}
       >
         <div className="w-full">
